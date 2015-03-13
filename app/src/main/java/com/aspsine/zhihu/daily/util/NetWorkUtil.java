11c019861afc8754cd946a22976c81aa8fa7737a
Aspsine <littleximail@gmail.com>
2015-03-13 17:32:36 +0800

    splash

---
+++ app/src/main/java/com/aspsine/zhihu/daily/util/NetWorkUtil.java
@@ -0,0 +1,13 @@
+package com.aspsine.zhihu.daily.util;
+
+import android.content.Context;
+
+/**
+ * Created by Aspsine on 2015/3/13.
+ */
+public class NetWorkUtil {
+    public static final boolean isNetWorkAvailable(Context c) {
+        return true;
+    }
+
+}

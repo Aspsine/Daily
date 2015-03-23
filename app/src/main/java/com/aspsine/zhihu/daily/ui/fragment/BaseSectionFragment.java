1728e0e4e10c05ae3e3fd4c828a56ed7b734151f
Aspsine <littleximail@gmail.com>
2015-03-23 09:47:13 +0800

    change title

--- app/src/main/java/com/aspsine/zhihu/daily/ui/fragment/BaseSectionFragment.java
+++ app/src/main/java/com/aspsine/zhihu/daily/ui/fragment/BaseSectionFragment.java
@@ -1,6 +1,10 @@
 package com.aspsine.zhihu.daily.ui.fragment;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+
+import com.aspsine.zhihu.daily.ui.activity.NavigationDrawerActivity;
 
 /**
  * Created by Aspsine on 2015/3/19.
@@ -14,5 +18,10 @@ public abstract class BaseSectionFragment extends Fragment {
 
     private int argSectionNumber;
 
-
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        argSectionNumber = getArguments().getInt(ARG_SECTION_NUMBER, 0);
+        ((NavigationDrawerActivity) getActivity()).onSectionAttached(argSectionNumber);
+    }
 }

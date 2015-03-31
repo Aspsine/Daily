b9cf0c774fbc15cc6a002a4a73cafa18764d59f8
Aspsine <littleximail@gmail.com>
2015-03-31 15:33:14 +0800

    delete unused code and demo code

---
+++ app/src/main/java/com/aspsine/zhihu/daily/network/OkHttp.java
@@ -0,0 +1,29 @@
+package com.aspsine.zhihu.daily.network;
+
+import android.content.Context;
+
+import com.aspsine.zhihu.daily.Constants;
+import com.aspsine.zhihu.daily.util.FileUtils;
+import com.squareup.okhttp.Cache;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Aspsine on 2015/3/31.
+ */
+public class OkHttp {
+    private static OkHttpClient okHttpClient;
+
+    public static OkHttpClient createHttpClient(Context context) {
+        if (okHttpClient == null) {
+            synchronized (OkHttp.class) {
+                okHttpClient = new OkHttpClient();
+                okHttpClient.setCache(new Cache(FileUtils.getHttpCacheDir(context), Constants.Config.HTTP_CACHE_SIZE));
+                okHttpClient.setConnectTimeout(Constants.Config.HTTP_CONNECT_TIMEOUT, TimeUnit.MILLISECONDS);
+                okHttpClient.setReadTimeout(Constants.Config.HTTP_READ_TIMEOUT, TimeUnit.MILLISECONDS);
+            }
+        }
+        return okHttpClient;
+    }
+}

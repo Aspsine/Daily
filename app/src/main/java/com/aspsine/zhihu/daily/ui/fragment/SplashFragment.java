e95a653ea6f79d1f19fe03bcfb5f1cbeff473b15
Aspsine <littleximail@gmail.com>
2015-08-16 10:26:21 +0800

    fix splash animation on mx5(flyme os system animation bug)

--- app/src/main/java/com/aspsine/zhihu/daily/ui/fragment/SplashFragment.java
+++ app/src/main/java/com/aspsine/zhihu/daily/ui/fragment/SplashFragment.java
@@ -1,6 +1,6 @@
 package com.aspsine.zhihu.daily.ui.fragment;
 
-import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -16,13 +16,16 @@ import android.widget.TextView;
 
 import com.aspsine.zhihu.daily.App;
 import com.aspsine.zhihu.daily.R;
+import com.aspsine.zhihu.daily.animation.AnimationEndListener;
 import com.aspsine.zhihu.daily.model.StartImage;
 import com.aspsine.zhihu.daily.respository.interfaces.Repository;
+import com.aspsine.zhihu.daily.ui.activity.GuiderActivity;
 import com.aspsine.zhihu.daily.util.DensityUtil;
 import com.aspsine.zhihu.daily.util.L;
 import com.aspsine.zhihu.daily.util.UIUtils;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
 /**
  * Created by sf on 2015/1/13.
@@ -36,20 +39,19 @@ public class SplashFragment extends Fragment {
     public ImageView ivSplash;
     public Animation mIvSplashAnim;
 
-    private DisplayImageOptions mOptions;
+    private final DisplayImageOptions mOptions = new DisplayImageOptions.Builder()
+            .cacheInMemory(false)
+            .cacheOnDisk(true)
+            .considerExifParams(true)
+            .build();
 
     private int mWidth;
     private int mHeight;
 
     @Override
-    public void onAttach(Activity activity) {
-        mIvSplashAnim = AnimationUtils.loadAnimation(activity, R.anim.splash);
-        super.onAttach(activity);
-    }
-
-    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mIvSplashAnim = AnimationUtils.loadAnimation(getActivity(), R.anim.splash);
 
         // if api >= 19 use themes in values-v19 has a full screen splash.
         mWidth = DensityUtil.getScreenWidth(getActivity());
@@ -59,11 +61,6 @@ public class SplashFragment extends Fragment {
             mHeight = DensityUtil.getScreenHeight(getActivity());
         }
         L.i(TAG, "screen height = " + mHeight);
-        this.mOptions = new DisplayImageOptions.Builder()
-                .cacheInMemory(false)
-                .cacheOnDisk(true)
-                .considerExifParams(true)
-                .build();
     }
 
     @Override
@@ -76,28 +73,27 @@ public class SplashFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         tvAuthor = (TextView) view.findViewById(R.id.tvAuthor);
         ivSplash = (ImageView) view.findViewById(R.id.splash);
-        ivLogo = (ImageView)view.findViewById(R.id.ivLogo);
+        ivLogo = (ImageView) view.findViewById(R.id.ivLogo);
 
         // if api >= 19 use themes in values-v19 has a full screen splash.
         // so we need relayout the tvAuthor and ivLogo by set a bigger marginBottom
         int navigationBarHeight = UIUtils.getNavigationBarHeight(getActivity());
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 
             int tvMarginBottom = navigationBarHeight + DensityUtil.dip2px(getActivity(), 8);
-            int logoMarginBottom = navigationBarHeight + DensityUtil.dip2px(getActivity() , 48);
+            int logoMarginBottom = navigationBarHeight + DensityUtil.dip2px(getActivity(), 48);
             ((RelativeLayout.LayoutParams) tvAuthor.getLayoutParams()).setMargins(0, 0, 0, tvMarginBottom);
             ((RelativeLayout.LayoutParams) ivLogo.getLayoutParams()).setMargins(0, 0, 0, logoMarginBottom);
 
             tvAuthor.requestLayout();
             ivLogo.requestLayout();
         }
-
-        ivSplash.startAnimation(mIvSplashAnim);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        mIvSplashAnim.setAnimationListener(animListener);
         refresh();
     }
 
@@ -107,6 +103,13 @@ public class SplashFragment extends Fragment {
         mIvSplashAnim = null;
     }
 
+    private final Animation.AnimationListener animListener = new AnimationEndListener() {
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            ivSplash.setVisibility(View.GONE);
+            ((GuiderActivity) getActivity()).intentToMainActivity();
+        }
+    };
 
     private void refresh() {
 
@@ -114,13 +117,25 @@ public class SplashFragment extends Fragment {
             @Override
             public void success(StartImage startImage, boolean outDate) {
                 tvAuthor.setText(startImage.getText());
-                ImageLoader.getInstance().displayImage(startImage.getImg(), ivSplash, mOptions);
+                ImageLoader.getInstance().displayImage(startImage.getImg(), ivSplash, mOptions, new SimpleImageLoadingListener(){
+                    @Override
+                    public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                        super.onLoadingComplete(imageUri, view, loadedImage);
+                        ivSplash.startAnimation(mIvSplashAnim);
+                    }
+                });
+
             }
 
             @Override
             public void failure(Exception e) {
                 L.i(TAG, "default image.");
-                ivSplash.setBackgroundResource(R.drawable.bg_splash);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    ivSplash.setBackground(getResources().getDrawable(R.drawable.bg_splash));
+                }else{
+                    ivSplash.setBackgroundDrawable(getResources().getDrawable(R.drawable.bg_splash));
+                }
+                ivSplash.setAnimation(mIvSplashAnim);
                 tvAuthor.setText(getResources().getString(R.string.splash_text));
                 e.printStackTrace();
             }
